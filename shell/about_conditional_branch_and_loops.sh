07cc959f9c13f5ac2009baa93496df076665c331
JUNoym <junnyaooyama@gmail.com>
2022-01-17 21:30:30 +0900

    変数名をNUMBERからnumberに変更

--- shell/about_conditional_branch_and_loops.sh
+++ shell/about_conditional_branch_and_loops.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
-NUMBER=${1} # 引数を取得 渡された数字が0になるまで1をひく
+number=${1} # 引数を取得 渡された数字が0になるまで1をひく
 while :
 do
-  if [ "$NUMBER" = "0" ]; then
+  if [ "$number" = "0" ]; then
     echo "0になったので,breakコマンドを実行"
     break
   else
-    echo "$NUMBERは0より大きいので,１つ減らす"
-    NUMBER=$((NUMBER-1))
+    echo "$numberは0より大きいので,１つ減らす"
+    number=$((number-1))
   fi
 done
 echo "ループを抜けました。"
